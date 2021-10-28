543c0a8b1ed00568a468e4b54455cc24ff565435
bastienlc <bastien.le-chenadec@eleves.enpc.fr>
2021-10-28 17:20:32 +0200

    starting apps and models

---
+++ back/social/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+
+from .models import Promotion, Student
+
+admin.site.register(Promotion)
+admin.site.register(Student)
---
+++ back/social/models.py
@@ -0,0 +1,43 @@
+from django.db import models
+from django.contrib.auth import models as models2
+from django.utils.translation import gettext_lazy as _
+from django.core.validators import RegexValidator
+
+class Promotion(models.Model):
+    year = models.IntegerField()
+
+class Student(models.Model):
+    user = models2.User()
+    promo = Promotion()
+
+    class Department(models.TextChoices):
+        IMI = 'IMI', _('Ingénierie mathématique et informatique')
+        GCC = 'GCC', _('Génie civil et construction')
+        GMM = 'GMM', _('Génie mécanique et matériaux')
+        SEGF = 'SEGF', _('Sciences économiques, gestion, finance')
+        VET = 'VET', _('Ville, environnement, transport')
+    department = models.CharField(
+        max_length=4,
+        choices=Department.choices,
+    )
+
+    class Gender(models.TextChoices):
+        F = 'Femme'
+        H = 'Homme'
+        A = 'Autre'
+    gender = models.CharField(
+        max_length=5,
+        choices=Gender.choices,
+        default = Gender.A
+    )
+
+    class Origin(models.TextChoices):
+        CC = 'Concours Commun'
+    origin = models.CharField(
+        max_length=20,
+        choices=Origin.choices,
+        default = Origin.CC
+    )
+
+    phone_regex = RegexValidator(regex=r'^\+?\d{9,16}$', message="Le numéro doit être entré au format: '+999999999'. Jusqu'à 16 chiffres sont autorisés.")
+    phone_number = models.CharField(validators=[phone_regex], max_length=17, blank=True) # validators should be a list
