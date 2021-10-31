776260d382a87befc116cc5ffa0326e359080fb3
bastienlc <bastien.le-chenadec@eleves.enpc.fr>
2021-11-01 00:57:06 +0100

    models for app trade

--- back/trade/admin.py
+++ back/trade/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Good, Transaction
+
+admin.site.register(Good)
+admin.site.register(Transaction)
--- back/trade/models.py
+++ back/trade/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+
+class Good(models.Model):
+    name = models.CharField(max_length=50)
+    price = models.IntegerField() #in 100th of euros
+    club = models.ForeignKey('social.Club', on_delete=models.CASCADE, null=True)
+
+
+class Transaction(models.Model):
+    good = models.ForeignKey('Good', on_delete=models.CASCADE)
+    quantity = models.IntegerField()
+    student = models.ForeignKey('social.Student', on_delete=models.CASCADE)
+    date = models.DateTimeField()
