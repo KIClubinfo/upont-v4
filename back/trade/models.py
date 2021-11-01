79b14a623a94cf89fe6acb5ba6f0a0967c9671fd
bastienlc <bastien.le-chenadec@eleves.enpc.fr>
2021-11-01 15:50:35 +0100

    check on_delete properties

--- back/trade/models.py
+++ back/trade/models.py
@@ -3,16 +3,18 @@ from django.db import models
 
 class Good(models.Model):
     name = models.CharField(max_length=50)
-    price = models.IntegerField() #in 100th of euros
-    club = models.ForeignKey('social.Club', on_delete=models.CASCADE)
+    price = models.IntegerField()  # in 100th of euros
+    club = models.ForeignKey("social.Club", on_delete=models.SET_NULL, null=True)
+
     def __str__(self):
         return self.name
 
 
 class Transaction(models.Model):
-    good = models.ForeignKey('Good', on_delete=models.CASCADE)
+    good = models.ForeignKey("Good", on_delete=models.SET_NULL, null=True)
     quantity = models.IntegerField()
-    student = models.ForeignKey('social.Student', on_delete=models.CASCADE)
+    student = models.ForeignKey("social.Student", on_delete=models.SET_NULL, null=True)
     date = models.DateTimeField()
+
     def __str__(self):
-        return self.good.name + ' : ' + self.student.user.username 
+        return self.good.name + " : " + self.student.user.username
