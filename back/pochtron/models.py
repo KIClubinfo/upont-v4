dd7610458e8780380223f929b5b3f7afaad42902
bastienlc <bastien.le-chenadec@eleves.enpc.fr>
2021-11-01 15:13:07 +0100

    get rid of floats

--- back/pochtron/models.py
+++ back/pochtron/models.py
@@ -3,7 +3,7 @@ from trade.models import Good
 
 
 class Alcohol(Good):
-    degree = models.FloatField()
-    volume = models.FloatField()
+    degree = models.DecimalField(max_digits=5, decimal_places=2)
+    volume = models.DecimalField(max_digits=6, decimal_places=3)
     def __str__(self):
         return self.name
