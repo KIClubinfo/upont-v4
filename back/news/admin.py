6090fd3ad04ebc27f21a42c2a6a3423284dd05be
bastienlc <bastien.le-chenadec@eleves.enpc.fr>
2021-11-03 10:58:49 +0100

    finish basic models and clean migrations

--- back/news/admin.py
+++ back/news/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Comment, Event, Post, Shotgun
+
+admin.site.register(Event)
+admin.site.register(Post)
+admin.site.register(Comment)
+admin.site.register(Shotgun)
--- back/news/models.py
+++ back/news/models.py
@@ -5,7 +5,7 @@ from social.models import Student
 class Event(models.Model):
     name = models.CharField(max_length=50)
     description = models.TextField()
-    club = models.ForeignKey("Club", on_delete=models.CASCADE)
+    club = models.ForeignKey("social.Club", on_delete=models.SET_NULL, null=True)
     date = models.DateTimeField()
     location = models.CharField(max_length=50)
     participants = models.ManyToManyField(
@@ -20,9 +20,14 @@ class Event(models.Model):
 
 class Post(models.Model):
     title = models.CharField(max_length=50)
-    author = models.ForeignKey("Club", verbose_name="author club", on_delete=models.CASCADE)
+    author = models.ForeignKey(
+        "social.Membership", verbose_name="author", on_delete=models.SET_NULL, null=True
+    )
+    published_as_student = models.BooleanField()
     date = models.DateTimeField()
-    illustration = models.ImageField(upload_to="post_illustrations", null=True, blank=True)
+    illustration = models.ImageField(
+        upload_to="post_illustrations", null=True, blank=True
+    )
     content = models.TextField()
     event = models.ForeignKey("Event", on_delete=models.SET_NULL, null=True, blank=True)
 
@@ -32,38 +37,29 @@ class Post(models.Model):
 
 class Comment(models.Model):
     post = models.ForeignKey("Post", on_delete=models.CASCADE)
-    author = models.ForeignKey("Student", on_delete=models.CASCADE)
+    author = models.ForeignKey(
+        "social.Membership", verbose_name="author", on_delete=models.SET_NULL, null=True
+    )
+    published_as_student = models.BooleanField()
     date = models.DateTimeField()
     content = models.TextField()
-    
+
     def __str__(self):
         return self.content
 
 
 class Shotgun(models.Model):
     title = models.CharField(max_length=50)
-    club = models.ForeignKey("Club", verbose_name="organizing club", on_delete=models.CASCADE)
+    club = models.ForeignKey(
+        "social.Club",
+        verbose_name="organizing club",
+        on_delete=models.SET_NULL,
+        null=True,
+    )
     content = models.TextField()
     date = models.DateTimeField()
     size = models.IntegerField(default=0)
     requires_motivation = models.BooleanField(default=False)
-    winners = models.ManyToManyField(
-        Student,
-        through="ShotgunResults",
-        related_name="successful shotguns",
-        blank=True,
-    )
 
     def __str__(self):
         return self.title
-
-
-class ShotgunResults(models.Model):
-    shotgun = models.ForeignKey(Shotgun, on_delete=models.CASCADE)
-    student = models.ForeignKey(Student, on_delete=models.SET_NULL, null=True)
-    position = models.IntegerField()
-    date = models.DateTimeField()
-    motivation = models.TextField()
-
-    def __str__(self):
-        return self.shotgun.title + " : " + self.student.user.username
