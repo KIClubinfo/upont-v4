db8fe5237dba303758159a32cdbe5dcc696a6700
Dadou15 <davidnahmias2000@gmail.com>
2021-11-04 01:52:12 +0100

    added admin and users pages + modif on login page

--- back/upont/views.py
+++ back/upont/views.py
@@ -7,3 +7,9 @@ def login(request):
 
 def index(request):
     return render(request, "index.html")
+
+def index_admin(request):
+    return render(request, "index_admin.html")
+
+def index_users(request):
+    return render(request, "index_users.html")
