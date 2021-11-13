757554fe5875640fcf497571319212a378f871bd
Louis-Bly <louis.boilly@gmail.com>
2021-11-13 17:33:12 +0100

    cleaner view, back ok, need template now

--- back/social/urls.py
+++ back/social/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
 from . import views
 
+
+app_name = 'social'
 urlpatterns = [
     path("index_users/", views.index_users),
     path("index_profile/", views.index_profile),
-    path("index_clubs/", views.index_clubs),
+    path("index_clubs/", views.index_clubs, name='club_index'),
+    path("view_club/<int:club_id>", views.view_club, name='club_detail'),
 ]
