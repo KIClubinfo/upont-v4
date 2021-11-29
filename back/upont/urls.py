d466be6b5b3c3df7c02cbdd184ad2fd406160b91
bastienlc <bastien.le-chenadec@eleves.enpc.fr>
2021-11-29 16:41:48 +0100

    display student pictures

--- back/upont/urls.py
+++ back/upont/urls.py
@@ -13,14 +13,21 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
+
 from . import views
 
 urlpatterns = [
     path("login/", views.login),
     path("", views.index),
     path("index_admin/", views.index_admin),
-    path("social/", include('social.urls')),
+    path("social/", include("social.urls")),
     path("admin/", admin.site.urls),
 ]
+
+# Only for dev, gives anyone access to any image
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
