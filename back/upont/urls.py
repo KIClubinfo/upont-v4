996806e6a9d0177be07d3a6c33303622f2c75e41
TheiloT <theilo.terrisse@gmail.com>
2021-12-06 15:21:42 +0100

    Clean including of authentication views

--- back/upont/urls.py
+++ back/upont/urls.py
@@ -14,7 +14,8 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+# from django.contrib.auth import views as auth_views
 from . import views
 
 urlpatterns = [
@@ -23,11 +24,5 @@ urlpatterns = [
     path("index_users/", views.index_users, name="index_users"),
     path("index_profil/", views.index_profil, name="index_profil"),
     path("admin/", admin.site.urls),
-    path('login/', views.Login.as_view(), name="login"),
-    path('password_change/', views.PasswordChange.as_view(), name="password_change"),
-    path('password_change_done/', views.PasswordChangeDone.as_view(), name="password_change_done"),
-    path('password_reset_form/', views.PasswordReset.as_view(), name="password_reset_form"),
-    path('password_reset_done/', views.PasswordResetDone.as_view(), name="password_reset_done"),
-    path('reset/<uidb64>/<token>/', views.PasswordResetConfirm.as_view(), name="password_reset_confirm"),
-    path('password_reset_complete/', views.PasswordResetComplete.as_view(), name="password_reset_complete"),
+    path('', include('django.contrib.auth.urls')),
 ]
--- back/upont/views.py
+++ back/upont/views.py
@@ -17,36 +17,3 @@ def index_users(request):
 
 def index_profil(request):
     return render(request, "index_profil.html")
-
-
-class Login(auth_views.LoginView):
-    template_name = "login.html"
-    extra_context = {"next": "/"}
-
-
-class PasswordChange(auth_views.PasswordChangeView):
-    template_name = "password_change.html"
-
-
-class PasswordChangeDone(auth_views.PasswordChangeDoneView):
-    template_name = "password_change_done.html"
-
-
-class PasswordReset(auth_views.PasswordResetView):
-    # For security reasons, if the inpu email does not exist in the system, the site will fail silently,
-    # ignoring the request.
-    template_name = "password_reset_form.html"
-    # email_template_name = "password_reset_email.html"
-    subject_template_name = "password_reset_subject.txt"
-
-
-class PasswordResetDone(auth_views.PasswordResetDoneView):
-    template_name = "password_reset_done.html"
-
-
-class PasswordResetConfirm(auth_views.PasswordResetConfirmView):
-    template_name = "password_reset_confirm.html"
-
-
-class PasswordResetComplete(auth_views.PasswordResetCompleteView):
-    template_name = "password_reset_complete.html"
