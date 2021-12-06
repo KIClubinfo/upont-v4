120577aef3036eca4fdc8e80aa5d2d5271b1fba6
Dadou15 <davidnahmias2000@gmail.com>
2021-12-06 17:03:02 +0100

    first integration commit

--- back/upont/urls.py
+++ back/upont/urls.py
@@ -22,12 +22,7 @@ from . import views
 
 urlpatterns = [
     path("login/", views.login),
-    path("", views.index),
-    path("index_admin/", views.index_admin),
     path("social/", include("social.urls")),
-    path("glasscard/", views.glasscard),
-    path("index_users/", views.index_users),
-    path("index_profil/", views.index_profil),
     path("admin/", admin.site.urls),
 ]
 
