4dddb2597adf80fbb71e62872d4aed2cb7a1a483
TheiloT <theilo.terrisse@gmail.com>
2021-12-06 15:38:25 +0100

    add logout

--- back/upont/urls.py
+++ back/upont/urls.py
@@ -15,7 +15,6 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-# from django.contrib.auth import views as auth_views
 from . import views
 
 urlpatterns = [
