6e6205de30791222bb7a94d45a2418ab2fc5f022
Thomas <thomasbuiron78@gmail.com>
2021-12-06 18:40:23 +0100

    View ok, need template

--- back/social/urls.py
+++ back/social/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
+
 from . import views
 
 urlpatterns = [
     path("index_users/", views.index_users),
+    path("index_profil/<int:student_id>/", views.index_profil, name="index_profil"),
 ]
