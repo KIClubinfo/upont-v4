41ad960b86c9026c2abb5b926701c66ef999657b
bastienlc <bastien.le-chenadec@eleves.enpc.fr>
2021-12-09 12:24:32 +0100

    added profil page

--- back/upont/views.py
+++ back/upont/views.py
@@ -5,8 +5,18 @@ from django.shortcuts import render
 def login(request):
     return render(request, "login.html")
 
+
 def index(request):
     return render(request, "index.html")
 
+
 def index_admin(request):
     return render(request, "index_admin.html")
+
+
+def index_users(request):
+    return render(request, "index_users.html")
+
+
+def index_profil(request):
+    return render(request, "index_profil.html")
