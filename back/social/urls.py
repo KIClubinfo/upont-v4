f9152be6f010741b03fd4845d8ac0c6152292d3f
Thomas <thomasbuiron78@gmail.com>
2021-12-10 09:48:23 +0100

    First integration of the template

--- back/social/urls.py
+++ back/social/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
+
 from . import views
 
 urlpatterns = [
     path("index_users/", views.index_users),
-    path("index_profile/", views.index_profile),
+    path("index_profile/<int:student_id>/", views.index_profile, name="index_profile"),
 ]
