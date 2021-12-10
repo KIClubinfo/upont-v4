ded23caa96bd69140a57f831bdda96c606971cf9
Dadou15 <davidnahmias2000@gmail.com>
2021-12-10 10:08:50 +0100

    fix

--- back/social/views.py
+++ back/social/views.py
@@ -1,5 +1,4 @@
 from django.shortcuts import render
-<<<<<<< HEAD
 from .models import Student
 
 
@@ -10,7 +9,3 @@ def index_users(request):
 
 def index_profile(request):
     return render(request, 'social/index_profile.html')
-=======
-
-# Create your views here.
->>>>>>> e00915f388ec79b9934fde3585c008937537fddc
