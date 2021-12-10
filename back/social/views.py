5ff4f73b559d485c5835990b5a87594c66651eb3
Thomas <thomasbuiron78@gmail.com>
2021-12-10 09:48:23 +0100

    First integration of the template

--- back/social/views.py
+++ back/social/views.py
@@ -1,11 +1,16 @@
-from django.shortcuts import render
-from .models import Student
+from django.shortcuts import get_object_or_404, render
+
+from .models import Membership, Student
 
 
 def index_users(request):
-    all_student_list = Student.objects.order_by('-promo__year', 'user__first_name')
-    context = {'all_student_list': all_student_list}
-    return render(request, 'social/index_users.html', context)
+    all_student_list = Student.objects.order_by("-promo__year", "user__first_name")
+    context = {"all_student_list": all_student_list}
+    return render(request, "social/index_users.html", context)
 
-def index_profile(request):
-    return render(request, 'social/index_profile.html')
+
+def index_profile(request, student_id):
+    student = get_object_or_404(Student, pk=student_id)
+    membership_club_list = Membership.objects.filter(student__pk=student_id)
+    context = {"student": student, "membership_club_list": membership_club_list}
+    return render(request, "social/index_profile.html", context)
