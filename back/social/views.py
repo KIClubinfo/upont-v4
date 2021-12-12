23a4b7ff8c466fd60af77b81ae23ad766e9d9578
TheiloT <theilo.terrisse@gmail.com>
2021-12-12 11:09:27 +0100

    More verbose on forms errors and redirections

--- back/social/views.py
+++ back/social/views.py
@@ -1,11 +1,15 @@
 from django.shortcuts import render
 from .models import Student
+from django.contrib.auth.decorators import login_required
 
 
+@login_required
 def index_users(request):
     all_student_list = Student.objects.order_by('-promo__year', 'user__first_name')
     context = {'all_student_list': all_student_list}
     return render(request, 'social/index_users.html', context)
 
+
+@login_required
 def index_profile(request):
     return render(request, 'social/index_profile.html')
