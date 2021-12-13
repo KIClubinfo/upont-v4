f9e14c6ae650c10d3aab5553686aaaf7bdd5b7ef
Louis-Bly <81046535+Louis-Bly@users.noreply.github.com>
2021-12-13 15:33:06 +0100

    View club merged with master, no template for now (#15)
    * Changed settings to gather all media in one folder
    
    * Simple version of club views, no template, fixed images
    
    * cleaner view, back ok, need template now
    
    * Simple version of club views, no template, fixed images
    
    * cleaner view, back ok, need template now
    
    * Simple version of club views, no template, fixed images
    
    * cleaner view, back ok, need template now
    
    * Authentication system (#17)
    
    * detailed git workflow in readme
    
    django useful commands
    
    black, isort hooks
    
    Apps and models (#2)
    
    * starting apps and models
    
    * first migrations
    
    * Update school departments
    
    Co-authored-by: Louis-Bly <81046535+Louis-Bly@users.noreply.github.com>
    
    * Update back/social/models.py
    
    * Update models.py
    
    * progress on models for app social
    
    * working models
    
    * change membership with manytomany field
    
    * models for app trade
    
    * models for app pochtron and changes to trade
    
    * finish social, trade and pochtron models
    
    * get rid of floats
    
    * check on_delete properties
    
    * migrations
    
    * update Alcohol's fields and Category's field name
    
    * Update model origin
    
    Co-authored-by: Theilo <81049450+TheiloT@users.noreply.github.com>
    
    * Create Views models
    
    * finish basic models and clean migrations
    
    Co-authored-by: Louis-Bly <81046535+Louis-Bly@users.noreply.github.com>
    Co-authored-by: dadou15 <81114663+dadou15@users.noreply.github.com>
    Co-authored-by: TheiloT <theilo.terrisse@gmail.com>
    Co-authored-by: Theilo <81049450+TheiloT@users.noreply.github.com>
    
    bug fix
    
    Changed settings to gather all media in one folder
    
    * implement login (intermediary commit)
    
    * Clean urls.py
    
    * Begin password resetting system
    
    * fix login with email
    
    * Small fix in login.html
    
    * Fix debug mode for emails
    
    * Password change and reset functional
    
    * login also with username
    
    * Clean including of authentication views
    
    * add logout
    
    * implement login (intermediary commit)
    
    * Clean urls.py
    
    * Begin password resetting system
    
    * fix login with email
    
    * Password change and reset functional
    
    * Clean including of authentication views
    
    * add logout
    
    * login also with username
    
    * rebase and new login template
    
    * fix
    
    * Login fix
    
    * Make email backend on debug mode
    
    * templates integration part 1
    
    * Expand forms rendering and small fixes
    
    * More verbose on forms errors and redirections
    
    * Small fix in password_reset_confirm.html
    
    * change form to div
    
    * Update password_reset_done.html
    
    * Small fix
    
    Co-authored-by: bastienlc <bastien.le-chenadec@eleves.enpc.fr>
    Co-authored-by: Dadou15 <davidnahmias2000@gmail.com>
    Co-authored-by: dadou15 <81114663+dadou15@users.noreply.github.com>
    
    * Simple version of club views, no template, fixed images
    
    * cleaner view, back ok, need template now
    
    * Simple version of club views, no template, fixed images
    
    * cleaner view, back ok, need template now
    
    * Simple version of club views, no template, fixed images
    
    * cleaner view, back ok, need template now
    
    * adapted for master, added login locks
    
    * added <html> for you David
    
    Co-authored-by: Dadou15 <davidnahmias2000@gmail.com>
    Co-authored-by: Theilo <81049450+TheiloT@users.noreply.github.com>
    Co-authored-by: bastienlc <bastien.le-chenadec@eleves.enpc.fr>
    Co-authored-by: dadou15 <81114663+dadou15@users.noreply.github.com>

--- back/social/views.py
+++ back/social/views.py
@@ -1,15 +1,29 @@
-from django.shortcuts import render
-from .models import Student
+from django.shortcuts import render, get_object_or_404
+from .models import Student, Club
 from django.contrib.auth.decorators import login_required
 
 
 @login_required
 def index_users(request):
-    all_student_list = Student.objects.order_by('-promo__year', 'user__first_name')
-    context = {'all_student_list': all_student_list}
-    return render(request, 'social/index_users.html', context)
+    all_student_list = Student.objects.order_by("-promo__year", "user__first_name")
+    context = {"all_student_list": all_student_list}
+    return render(request, "social/index_users.html", context)
 
 
 @login_required
 def index_profile(request):
-    return render(request, 'social/index_profile.html')
+    return render(request, "social/index_profile.html")
+
+
+@login_required
+def index_clubs(request):
+    all_clubs_list = Club.objects.order_by("name")
+    context = {"all_clubs_list": all_clubs_list}
+    return render(request, "social/index_clubs.html", context)
+
+
+@login_required
+def view_club(request, club_id):
+    club = get_object_or_404(Club, pk=club_id)
+    context = {"club": club}
+    return render(request, "social/view_club.html", context)
