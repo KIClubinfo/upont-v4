e3e56a055bf9b68785643f52d808f38710578394
Thomas <thomasbuiron78@gmail.com>
2021-12-13 15:41:45 +0100

    Pull request

--- back/social/urls.py
+++ back/social/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 app_name = "social"
 urlpatterns = [
-    path("index_users/", views.index_users),
+    path("index_users/", views.index_users, name="index_users"),
     path("index_profile/<int:student_id>/", views.index_profile, name="index_profile"),
     path("index_clubs/", views.index_clubs, name="club_index"),
     path("view_club/<int:club_id>", views.view_club, name="club_detail"),
--- back/social/views.py
+++ back/social/views.py
@@ -3,14 +3,14 @@ from django.shortcuts import get_object_or_404, render
 from .models import Club, Membership, Student
 
 
-@login_required
+@login_required(login_url="/login/")
 def index_users(request):
     all_student_list = Student.objects.order_by("-promo__year", "user__first_name")
     context = {"all_student_list": all_student_list}
     return render(request, "social/index_users.html", context)
 
 
-@login_required
+@login_required(login_url="/login/")
 def index_profile(request, student_id):
     student = get_object_or_404(Student, pk=student_id)
     membership_club_list = Membership.objects.filter(student__pk=student_id)
