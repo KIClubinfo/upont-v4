a64c9f62a188c5bc9673af36e8bd05b1e4ceb087
Thomas <thomasbuiron78@gmail.com>
2021-12-13 15:41:45 +0100

    Pull request

--- back/social/views.py
+++ back/social/views.py
@@ -1,14 +1,17 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import get_object_or_404, render
 
 from .models import Membership, Student
 
 
+@login_required(login_url="/login/")
 def index_users(request):
     all_student_list = Student.objects.order_by("-promo__year", "user__first_name")
     context = {"all_student_list": all_student_list}
     return render(request, "social/index_users.html", context)
 
 
+@login_required(login_url="/login/")
 def index_profile(request, student_id):
     student = get_object_or_404(Student, pk=student_id)
     membership_club_list = Membership.objects.filter(student__pk=student_id)
