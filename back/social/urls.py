7775e2534e8c628326e7946f87242e49b4550309
Thomas <thomasbuiron78@gmail.com>
2021-12-13 15:41:45 +0100

    Pull request

--- back/social/urls.py
+++ back/social/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("index_users/", views.index_users),
+    path("index_users/", views.index_users, name="index_users"),
     path("index_profile/<int:student_id>/", views.index_profile, name="index_profile"),
 ]
