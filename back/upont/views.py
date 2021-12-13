82942a4ce44eeecf9622a8971b47c057d9668624
Theilo <81049450+TheiloT@users.noreply.github.com>
2021-12-13 14:47:14 +0100

    Authentication system (#17)
    * detailed git workflow in readme
    
    django useful commands
    
    black, isort hooks
    
    Apps and models (#2)
    
    * starting apps and models
    
    * first migrations
    
    * Update school departments
    
    Co-authored-by: Louis-Bly <81046535+Louis-Bly@users.noreply.github.com>
    
    * Update back/social/models.py
    
    * Update models.py
    
    * progress on models for app social
    
    * working models
    
    * change membership with manytomany field
    
    * models for app trade
    
    * models for app pochtron and changes to trade
    
    * finish social, trade and pochtron models
    
    * get rid of floats
    
    * check on_delete properties
    
    * migrations
    
    * update Alcohol's fields and Category's field name
    
    * Update model origin
    
    Co-authored-by: Theilo <81049450+TheiloT@users.noreply.github.com>
    
    * Create Views models
    
    * finish basic models and clean migrations
    
    Co-authored-by: Louis-Bly <81046535+Louis-Bly@users.noreply.github.com>
    Co-authored-by: dadou15 <81114663+dadou15@users.noreply.github.com>
    Co-authored-by: TheiloT <theilo.terrisse@gmail.com>
    Co-authored-by: Theilo <81049450+TheiloT@users.noreply.github.com>
    
    bug fix
    
    Changed settings to gather all media in one folder
    
    * implement login (intermediary commit)
    
    * Clean urls.py
    
    * Begin password resetting system
    
    * fix login with email
    
    * Small fix in login.html
    
    * Fix debug mode for emails
    
    * Password change and reset functional
    
    * login also with username
    
    * Clean including of authentication views
    
    * add logout
    
    * implement login (intermediary commit)
    
    * Clean urls.py
    
    * Begin password resetting system
    
    * fix login with email
    
    * Password change and reset functional
    
    * Clean including of authentication views
    
    * add logout
    
    * login also with username
    
    * rebase and new login template
    
    * fix
    
    * Login fix
    
    * Make email backend on debug mode
    
    * templates integration part 1
    
    * Expand forms rendering and small fixes
    
    * More verbose on forms errors and redirections
    
    * Small fix in password_reset_confirm.html
    
    * change form to div
    
    * Update password_reset_done.html
    
    * Small fix
    
    Co-authored-by: bastienlc <bastien.le-chenadec@eleves.enpc.fr>
    Co-authored-by: Dadou15 <davidnahmias2000@gmail.com>
    Co-authored-by: dadou15 <81114663+dadou15@users.noreply.github.com>

--- back/upont/views.py
+++ back/upont/views.py
@@ -1,6 +1,3 @@
+from django.contrib.auth import views as auth_views
 from django.http import HttpResponse
 from django.shortcuts import render
-
-
-def login(request):
-    return render(request, "login.html")
