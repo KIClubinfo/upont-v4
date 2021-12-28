6e8f5665528d9c9b43265e8d4e59b241222967ea
Thomas <thomasbuiron78@gmail.com>
2021-12-28 22:07:09 +0100

    First version of the search bar

--- back/social/urls.py
+++ back/social/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     path("index_users/", views.index_users, name="index_users"),
     path("profile/", views.profile, name="profile"),
     path("profile/<int:student_id>/", views.profile, name="profile_viewed"),
+    path("search/", views.search, name="search"),
     path("profile_edit/", views.profile_edit, name="profile_edit"),
     path("index_clubs/", views.index_clubs, name="club_index"),
     path("view_club/<int:club_id>", views.view_club, name="club_detail"),
