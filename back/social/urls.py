341eebbbe35138e6bc7e11b3df65e3346cfcf627
Louis-Bly <louis.boilly@gmail.com>
2022-01-01 17:15:31 +0100

    added club search, starting to set club editing

--- back/social/urls.py
+++ back/social/urls.py
@@ -6,8 +6,9 @@ urlpatterns = [
     path("index_users/", views.index_users, name="index_users"),
     path("profile/", views.profile, name="profile"),
     path("profile/<int:student_id>/", views.profile, name="profile_viewed"),
-    path("search/", views.search, name="search"),
+    path("search", views.search, name="search"),
     path("profile_edit/", views.profile_edit, name="profile_edit"),
     path("index_clubs/", views.index_clubs, name="club_index"),
     path("view_club/<int:club_id>", views.view_club, name="club_detail"),
+    path("club_edit/<int:club_id>", views.club_edit, name="club_edit"),
 ]
