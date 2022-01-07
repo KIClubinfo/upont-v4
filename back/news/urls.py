9a14aac46abf3d000e33546eebe1350e5711865e
Thomas <thomasbuiron78@gmail.com>
2022-01-07 20:04:42 +0100

    fix typo

--- back/news/urls.py
+++ back/news/urls.py
@@ -5,6 +5,6 @@ from . import views
 app_name = "news"
 urlpatterns = [
     path("posts/", views.posts, name="posts"),
-    path("events/idex", views.events, name="events"),
+    path("events/index", views.events, name="events"),
     path("event/<int:event_id>/detail", views.event_detail, name="event_detail"),
 ]
