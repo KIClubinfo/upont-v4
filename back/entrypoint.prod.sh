65fb42baec09d1b970a6fac52847c3912f1ea7de
bastienlc <bastien.le-chenadec@eleves.enpc.fr>
2022-01-13 22:29:00 +0100

    add nginx, works like a charm !

--- back/entrypoint.prod.sh
+++ back/entrypoint.prod.sh
@@ -8,4 +8,7 @@ done
 
 echo "PostgreSQL started"
 
+python3 manage.py migrate --noinput
+python3 manage.py collectstatic --noinput
+
 exec "$@"
--- back/upont/settings.py
+++ back/upont/settings.py
@@ -154,7 +154,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = "/static/"
-STATIC_ROOT = os.path.join(BASE_DIR, "../static/")
+STATIC_ROOT = os.path.join(BASE_DIR, "static/")
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "upont/static"),
 ]
@@ -168,7 +168,7 @@ FIXTURE_DIRS = ["/fixtures/"]
 
 # Default media folder
 MEDIA_URL = "/media/"
-MEDIA_ROOT = os.path.join(BASE_DIR, "../media/")
+MEDIA_ROOT = os.path.join(BASE_DIR, "media/")
 
 # Email backend
 if DEBUG:
