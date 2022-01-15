1c7ab94634bc95b6eebe63333315129a62cc7296
bastienlc <78959054+bastienlc@users.noreply.github.com>
2022-01-15 01:51:54 +0100

    SSO using django-cas-ng (#34)
    * Add SSO
    
    * Fix SSO - Attempt 1
    
    * Fix add_promo using wrong usernames
    
    * Automatically redirect authenticated users

--- back/upont/urls.py
+++ back/upont/urls.py
@@ -13,9 +13,11 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+import django_cas_ng.views
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
+from django.contrib.auth import views as auth_views
 from django.urls import include, path
 from upont.add_promo import add
 
@@ -26,8 +28,15 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("tellme/", include("tellme.urls"), name="tellme"),
     path("add_promo/", add),
+    path(
+        "login/",
+        auth_views.LoginView.as_view(redirect_authenticated_user=True),
+        name="login",
+    ),  # forces redirection of already authenticated users
     path("", include("django.contrib.auth.urls")),
     path("", views.root_redirect),
+    path("cas/login", django_cas_ng.views.LoginView.as_view(), name="cas_ng_login"),
+    path("cas/logout", django_cas_ng.views.LogoutView.as_view(), name="cas_ng_logout"),
 ]
 
 if settings.DEBUG:  # in debug anyone can access any image
