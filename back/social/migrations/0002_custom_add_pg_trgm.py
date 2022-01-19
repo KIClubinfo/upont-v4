59a911b12fbf3c9827fa32a5b43c5755916267b6
bastienlc <78959054+bastienlc@users.noreply.github.com>
2022-01-19 14:04:00 +0100

    Clean all migrations (#41)

--- back/social/migrations/0002_custom_add_pg_trgm.py
+++ back/social/migrations/0002_custom_add_pg_trgm.py
@@ -1,10 +1,11 @@
+# This migration was not generated automatically. Be careful that you DON'T DELETE IT !
 from django.contrib.postgres.operations import TrigramExtension
 from django.db import migrations
 
 
 class Migration(migrations.Migration):
     dependencies = [
-        ("social", "0003_alter_club_nickname"),
+        ("social", "0001_initial"),
     ]
 
     operations = [
