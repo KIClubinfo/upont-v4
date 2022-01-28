11b8cc400f91f31cec6bf7876fb7e3b2e306eab2
bastienlc <bastien.le-chenadec@eleves.enpc.fr>
2022-01-28 13:06:38 +0100

    Using a single Dockerfile with multi-stages build and switching to Poetry to lock dependancies

---
+++ back/entrypoint.dev.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+echo "Waiting for postgres..."
+
+while ! nc -z $DB_HOST $DB_PORT; do
+    sleep 0.1
+done
+
+echo "PostgreSQL started"
+
+python3 manage.py migrate --noinput
+python3 manage.py test
+
+exec "$@"
