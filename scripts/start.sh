d47b17a9cf2ca5d9316af0552231eb99cf522eff
bastienlc <bastien.le-chenadec@eleves.enpc.fr>
2022-02-20 20:22:19 +0100

    Fix build in prod ?

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,1 +1,1 @@
-docker-compose -f docker-compose_prod.yml run webinstaller && docker-compose -f docker-compose_prod.yml up back -d
+docker-compose -f docker-compose_prod.yml run webinstaller && docker-compose -f docker-compose_prod.yml up -d
