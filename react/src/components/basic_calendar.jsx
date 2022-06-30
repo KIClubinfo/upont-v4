a5766783b9e581eed248080aff89349b1584f86d
Ludovic Druette <ludovic.druette@protonmail.com>
2022-06-30 22:42:11 +0200

    Connected the calendar with the db

--- react/src/components/basic_calendar.jsx
+++ react/src/components/basic_calendar.jsx
@@ -1,4 +1,5 @@
 import React, { Fragment, useMemo } from 'react'
+import { useAsync } from 'react-async'
 import PropTypes from 'prop-types'
 import moment from 'moment'
 
@@ -10,10 +11,43 @@ import {
 } from 'react-big-calendar'
 
 import DemoLink from './calendar_example/DemoLink.component.js'
-import events from './calendar_example/event'
 import * as dates from './calendar_example/dates'
 
 const mLocalizer = momentLocalizer(moment)
+
+async function getEvents () {
+  const response = await fetch('/api/events')
+    .then(
+      result => {
+        return result.json()
+      },
+      error => {
+        console.error('Error getting the events: ' + error)
+      }
+    )
+
+  const rawEvents = response.results
+
+  const addOneHour = d => {
+    d.setHours(d.getHours() + 1)
+    return d
+  }
+
+  const formattedEvents = [] // Events formatted for BigCalendar
+  for (const e of rawEvents) {
+    formattedEvents.push(
+      {
+        id: e.id,
+        title: e.name,
+        desc: e.description,
+        start: new Date(e.date),
+        end: addOneHour(new Date(e.date))
+      }
+    )
+  }
+
+  return formattedEvents
+}
 
 const ColoredDateCellWrapper = ({ children }) =>
   React.cloneElement(React.Children.only(children), {
@@ -37,12 +71,17 @@ export default function Basic ({
       components: {
         timeSlotWrapper: ColoredDateCellWrapper
       },
-      defaultDate: new Date(2015, 3, 1),
+      defaultDate: new Date(2022, 3, 1),
       max: dates.add(dates.endOf(new Date(2015, 17, 1), 'day'), -1, 'hours'),
       views: Object.keys(Views).map((k) => Views[k])
     }),
     []
   )
+
+  const { data, error } = useAsync({ promiseFn: getEvents })
+  if (error) {
+    console.log(error.message)
+  }
 
   return (
     <>
@@ -50,7 +89,7 @@ export default function Basic ({
         <Calendar
           components={components}
           defaultDate={defaultDate}
-          events={events}
+          events={data}
           localizer={localizer}
           max={max}
           showMultiDayTimes
