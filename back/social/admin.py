410d8a3bf37d50d76a2b72630b350e056b317354
sachabinder <binder.sacha@gmail.com>
2022-09-11 00:18:36 +0200

    django admin update

--- back/social/admin.py
+++ back/social/admin.py
@@ -14,17 +14,27 @@ from .models import (
 
 class StudentAdmin(admin.ModelAdmin):
     list_display = ("user", "promo", "department")
+    list_filter = ("promo", "department")
 
 
 class ClubAdmin(admin.ModelAdmin):
     list_display = ("name",)
 
 
+class MembershipsAdmin(admin.ModelAdmin):
+    list_display = ("student", "club", "role")
+    list_filter = ("club", "role")
+
+
+class ClubRequestAdmin(admin.ModelAdmin):
+    list_display = ("name", "student")
+
+
 admin.site.register(Promotion)
 admin.site.register(Student, StudentAdmin)
 admin.site.register(Nationality)
 admin.site.register(Role)
-admin.site.register(Membership)
+admin.site.register(Membership, MembershipsAdmin)
 admin.site.register(Category)
 admin.site.register(Club, ClubAdmin)
-admin.site.register(ClubRequest)
+admin.site.register(ClubRequest, ClubRequestAdmin)
