ec6e4595999746b1e9e2117038e02d5fdd28d993
hectormartinet <hectormartinet@yahoo.fr>
2022-11-21 14:59:02 +0100

    erase test

--- back/courses/urls.py
+++ back/courses/urls.py
@@ -1,9 +1,3 @@
-from django.urls import path
-
-from . import views
-
 app_name = "course"
 
-urlpatterns = [
-    path("", views.test_to_delete, name="test"),
-]
+urlpatterns = []
--- back/courses/views.py
+++
@@ -1,5 +0,0 @@
-from django.http import HttpResponse
-
-
-def test_to_delete(request):
-    return HttpResponse("It works !")
