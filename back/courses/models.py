18b39e02fe087bea0422ff653d6b2029639251db
audreyairaud <audrey.airaud@gmail.com>
2022-11-28 14:16:46 +0100

    add_class_ressources

--- back/courses/models.py
+++ back/courses/models.py
@@ -1,1 +1,9 @@
-# Create your models here.
+class Ressources(models.Model):
+    name=models.CharField(max_length=50, default="Ressource")
+    author = models.ForeignKey(
+        "social.Student", verbose_name="author", on_delete=models.SET_NULL, null=True
+    )
+    date = models.DateTimeField()
+    file= FileField(
+        "Fichier",upload_to='ressources', null=TRUE,blank=TRUE
+        )
