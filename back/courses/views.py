e6d5117c1a9fea91a6cbac85bf101643229a8f0e
audreyairaud <audrey.airaud@gmail.com>
2022-11-28 15:47:15 +0100

    essai

--- back/courses/views.py
+++ back/courses/views.py
@@ -15,21 +15,7 @@ from .models import Course
 @login_required
 def view_course(request, course):
     course = get_object_or_404(Course, pk=course_id)
-    active_members = Membership.objects.filter(club__id=club_id, is_old=False)
-    old_members = get_old_members(club_id)
-    membership_club_list = Membership.objects.filter(
-        student__user__id=request.user.id, club__pk=club_id
-    )
-    if not membership_club_list:  # If no match is found
-        is_admin = False
-    elif not membership_club_list[0].is_admin:  # If the user does not have the rights
-        is_admin = False
-    else:
-        is_admin = True
     context = {
-        "club": club,
-        "active_members": active_members,
-        "old_members": old_members,
-        "is_admin": is_admin,
+        "couse": course,
     }
-    return render(request, "social/view_club.html", context)
+    return render(request, "courses/view_course.html", context)
