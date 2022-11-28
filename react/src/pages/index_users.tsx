7793cec344bb2de2b89318cbc259bd70894878d1
Ludovic Druette <ludovicdruette@protonmail.com>
2022-11-28 15:23:20 +0100

    Lint front files

--- react/src/pages/index_users.tsx
+++ react/src/pages/index_users.tsx
@@ -1,5 +1,5 @@
-import { render } from 'react-dom'
-import { Students } from '../components/StudentsPage/StudentsComponent'
+import { render } from 'react-dom';
+import { Students } from '../components/StudentsPage/StudentsComponent';
 
 //@ts-ignore window.react_mount is declared in django template
 render(<Students />, window.react_mount);
