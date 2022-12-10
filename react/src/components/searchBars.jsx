78722a27aa43b6cd8b0206b2e073e4011eff94de
Ludovic Druette <ludovicdruette@protonmail.com>
2022-12-10 19:41:47 +0100

    Fix eslint errors for searchBars.jsx

--- react/src/components/searchBars.jsx
+++ react/src/components/searchBars.jsx
@@ -1,27 +1,26 @@
+/* eslint-disable react/prop-types */
+/* eslint-disable max-classes-per-file */
 import React from 'react';
 import AsyncSelect from 'react-select/async';
 
+function searchStudent(inputValue) {
+  return new Promise((resolve, reject) => {
+    // eslint-disable-next-line no-undef
+    fetch(`${Urls.search_students()}?user=${inputValue}`)
+      .then((response) => response.json())
+      .then(({ students }) => {
+        resolve(
+          students.map(({ id, user, promo }) => ({
+            value: id,
+            label: `${user.first_name} ${user.last_name} ('${promo.nickname})`,
+          })),
+        );
+      })
+      .catch(reject);
+  });
+}
+
 class StudentsSearchBar extends React.Component {
-  constructor(props) {
-    super(props);
-  }
-
-  getStudent(inputValue) {
-    return new Promise((resolve, reject) => {
-      fetch(Urls['search_students']() + `?user=${inputValue}`)
-        .then((response) => response.json())
-        .then(({ students }) => {
-          resolve(
-            students.map(({ id, user, promo }) => ({
-              value: id,
-              label: `${user.first_name} ${user.last_name} ('${promo.nickname})`,
-            })),
-          );
-        })
-        .catch(reject);
-    });
-  }
-
   customStyles = {
     option: (provided) => ({
       ...provided,
@@ -47,36 +46,33 @@ class StudentsSearchBar extends React.Component {
         loadingMessage={() => 'Chargement'}
         onChange={this.handleChange}
         placeholder="Sélectionner un élève"
-        loadOptions={this.getStudent}
+        loadOptions={searchStudent}
         value={this.props.parent.state.student}
       />
     );
   }
 }
 
+function searchAlcohol(inputValue) {
+  return new Promise((resolve, reject) => {
+    // eslint-disable-next-line no-undef
+    fetch(`${Urls.search_alcohols()}?alcohol=${inputValue}`)
+      .then((response) => response.json())
+      .then(({ alcohols }) => {
+        resolve(
+          alcohols.map(({ name, degree, volume, price, id }) => ({
+            value: id,
+            label: `${name} (${volume}mL - ${degree}% - ${(price / 100).toFixed(
+              2,
+            )} €})`,
+          })),
+        );
+      })
+      .catch(reject);
+  });
+}
+
 class AlcoholsSearchBar extends React.Component {
-  constructor(props) {
-    super(props);
-  }
-
-  getAlcohol(inputValue) {
-    return new Promise((resolve, reject) => {
-      fetch(Urls['search_alcohols']() + `?alcohol=${inputValue}`)
-        .then((response) => response.json())
-        .then(({ alcohols }) => {
-          resolve(
-            alcohols.map(({ name, degree, volume, price, id }) => ({
-              value: id,
-              label: `${name} (${volume}mL - ${degree}% - ${(
-                price / 100
-              ).toFixed(2)} €})`,
-            })),
-          );
-        })
-        .catch(reject);
-    });
-  }
-
   customStyles = {
     option: (provided) => ({
       ...provided,
@@ -102,7 +98,7 @@ class AlcoholsSearchBar extends React.Component {
         loadingMessage={() => 'Chargement'}
         onChange={this.handleChange}
         placeholder="Sélectionner une consommation"
-        loadOptions={this.getAlcohol}
+        loadOptions={searchAlcohol}
         value={this.props.parent.state.alcohol}
       />
     );
