3de3bea191fe4f56c5a798dc370fa7902ad1325a
RedaBenDjelloun <reda.bendjelloun@gmail.com>
2022-12-12 14:32:48 +0100

    Changes after review

--- back/courses/models.py
+++ back/courses/models.py
@@ -1,28 +1,41 @@
-from courses.models import Course
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 from news.models import Post
 from social.models import Student
 
 
-class Update(models.Model):
-    date = models.DateTimeField()
-    old_course = models.ForeignKey(Course, on_delete=models.CASCADE)
-    new_course = models.ForeignKey(Course, on_delete=models.CASCADE)
+class Teacher(models.Model):
+    name = models.CharField(max_length=100, default="Professeur anonyme")
 
-    def __str__(self):
-        return self.old_course.name + " : " + self.new_course.name
+
+class CourseDepartment(models.TextChoices):
+    IMI = "IMI", _("Ingénierie mathématique et informatique")
+    GCC = "GCC", _("Génie civil et construction")
+    GMM = "GMM", _("Génie mécanique et matériaux")
+    SEGF = "SEGF", _("Sciences économiques, gestion, finance")
+    VET = "VET", _("Ville, environnement, transport")
+    GI = "GI", _("Génie industriel")
+    A1 = "1A", _("Première année")
+    DE = "DE", _("Direction de l'enseignement")
+    DLC = "DLC", _("Département langues et culture")
+    SHS = "SHS", _("Sciences humaines et sociales")
+    PAPDD = "PAPDD", _("Politique et action publique pour le développement durable")
+    DS = "D.SCHOOL", _("d.school")
+    AHE = "AHE", _("Autres hors école")
 
 
 class Course(models.Model):
-    name = models.CharField(max_length=100, default="Cours inconnu")
+    name = models.CharField(max_length=100, default="Cours sans nom")
     acronym = models.CharField(max_length=100, default="ABC")
     department = models.CharField(
-        max_length=4, choices=Student.Department.choices, default=Student.Department.A1
+        max_length=8, choices=CourseDepartment.choices, default=CourseDepartment.AHE
     )
+    head = models.ForeignKey(Teacher, on_delete=models.CASCADE)
     description = models.TextField()
     old_courses = models.ManyToManyField(
-        Course,
+        "self",
         through="Update",
+        symmetrical=False,
         related_name="new_courses",
         blank=True,
     )
@@ -33,17 +46,13 @@ class Course(models.Model):
     )
 
 
-class Teacher(models.Model):
-    name = models.CharField(max_length=100, default="Professeur inconnu")
-
-
-class Enrolment(models.Model):
-    is_old = models.BooleanField(default=False)
-    course = models.ForeignKey(Course, on_delete=models.CASCADE)
-    student = models.ForeignKey(Student, on_delete=models.CASCADE)
+class Update(models.Model):
+    date = models.DateTimeField()
+    old_course = models.ForeignKey(Course, on_delete=models.CASCADE, related_name="old")
+    new_course = models.ForeignKey(Course, on_delete=models.CASCADE, related_name="new")
 
     def __str__(self):
-        return self.course.name + " : " + self.student.user.username
+        return self.old_course.name + " : " + self.new_course.name
 
 
 class Group(models.Model):
@@ -55,3 +64,12 @@ class Group(models.Model):
         related_name="course",
         blank=True,
     )
+
+
+class Enrolment(models.Model):
+    is_old = models.BooleanField(default=False)
+    course = models.ForeignKey(Group, on_delete=models.CASCADE)
+    student = models.ForeignKey(Student, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.course.name + " : " + self.student.user.username
