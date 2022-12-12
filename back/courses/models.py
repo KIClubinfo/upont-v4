47cfd69a3c87b24492d2e9919e8dc38d173c8db5
audreyairaud <audrey.airaud@gmail.com>
2022-12-12 14:07:15 +0100

    changes_librairies

--- back/courses/models.py
+++ back/courses/models.py
@@ -1,14 +1,8 @@
-from io import BytesIO
-from uuid import uuid4
-
 from django.contrib.auth import models as models2
 from django.core.files import File
-from django.core.validators import RegexValidator
 from django.db import models
-from django.utils.translation import gettext_lazy as _
-from PIL import Image
-from trade.models import Transaction
-from unidecode import unidecode
+
+
 
 class Ressources(models.Model):
     name=models.CharField(max_length=50, default="Ressource")
