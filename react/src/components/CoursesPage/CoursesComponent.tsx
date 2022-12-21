4d0889bbd612460b1c949de1344a0a98e586eed7
Ludovic Druette <ludovicdruette@protonmail.com>
2022-12-21 21:00:38 +0100

    Add the possibility to see only the courses where the login student is enrolled

--- react/src/components/CoursesPage/CoursesComponent.tsx
+++ react/src/components/CoursesPage/CoursesComponent.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import { setDatasets } from 'react-chartjs-2/dist/utils';
 import InfiniteScroll from 'react-infinite-scroller';
 import { fetchPaginatedData } from '../utils/utils'
 import { Course } from './CourseComponent';
@@ -8,20 +9,39 @@ export const Courses: React.FC = () => {
     // @ts-ignore Urls is declared in the django template
     const [nextUrl, setNextUrl] = useState(Urls.courseList());
     const [moreExists, setMoreExists] = useState(true);
+    const [activeTab, setActiveTab] = useState(1);
 
     const fetchData = fetchPaginatedData(courses, setCourses, setMoreExists, nextUrl, setNextUrl);
 
+    const handleChangeTab = (tabNumber, urlArg="") => () => {
+        setCourses([]);
+        // @ts-ignore Urls is declared in the django template
+        setNextUrl(urlArg ? `${Urls.courseList()}?${urlArg}` : Urls.courseList());
+        setMoreExists(true);
+        setActiveTab(tabNumber)
+    }
+
     return (
-        // <>
-        // <p>Test</p>
-        // <Course course={{name:"Nom", acronym:"ACR", department:"GMM"}} />
-        // </>
-        <InfiniteScroll
-            loadMore={fetchData}
-            hasMore={moreExists}
-            loader={<div key="-1">Loading...</div>}
-            >
-        {courses.map((course) => (<Course course={course} key={course.id} />))}
-        </InfiniteScroll>
+        <>
+            <div className="row tab">
+                <div className="col">
+                    <button className={activeTab == 1 ? "active" : ""} onClick={handleChangeTab(1)} type="button">
+                            Tous les cours
+                    </button>
+                </div>
+                <div className="col">
+                    <button className={activeTab == 2 ? "active": ""} onClick={handleChangeTab(2, "is_enrolled=true")} type="button">
+                        Mes cours
+                    </button>
+                </div>
+            </div>
+            <InfiniteScroll
+                loadMore={fetchData}
+                hasMore={moreExists}
+                loader={<div key="-1">Loading...</div>}
+                >
+            {courses.map((course) => (<Course course={course} key={course.id} />))}
+            </InfiniteScroll>
+        </>
     )
 }
