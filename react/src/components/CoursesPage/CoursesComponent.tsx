6f928c20c79c7d186aa1914cf23728229e803ed9
Ludovic Druette <ludovicdruette@protonmail.com>
2022-12-22 22:52:11 +0100

    Add departement filtering on courses index

--- react/src/components/CoursesPage/CoursesComponent.tsx
+++ react/src/components/CoursesPage/CoursesComponent.tsx
@@ -1,36 +1,106 @@
-import React, { useState } from 'react';
-import { setDatasets } from 'react-chartjs-2/dist/utils';
+import React, { useState, useEffect } from 'react';
 import InfiniteScroll from 'react-infinite-scroller';
+import AsyncSelect from 'react-select/async';
 import { fetchPaginatedData } from '../utils/utils'
 import { Course } from './CourseComponent';
 
+const selectStyle = {
+    option: (base) => ({
+        ...base,
+        // backgroundColor: '#1A1B20',
+        // borderColor: '#1A1B20'
+        color: 'black'
+    })
+}
+
 export const Courses: React.FC = () => {
+    const tabs = {
+        ALL_COURSE: 1,
+        ONLY_ENROLLED: 2,
+    };
+
     const [courses, setCourses] = useState([]);
     // @ts-ignore Urls is declared in the django template
     const [nextUrl, setNextUrl] = useState(Urls.courseList());
+    const [urlParams, setUrlParams] = useState(new URLSearchParams);
     const [moreExists, setMoreExists] = useState(true);
-    const [activeTab, setActiveTab] = useState(1);
+    const [activeTab, setActiveTab] = useState(tabs.ALL_COURSE);
 
     const fetchData = fetchPaginatedData(courses, setCourses, setMoreExists, nextUrl, setNextUrl);
 
-    const handleChangeTab = (tabNumber, urlArg="") => () => {
+    const getDepartments = () => new Promise((resolve, reject) => {
+        // @ts-ignore Urls is declared in the django template
+        fetch(Urls.courseDepartmentList())
+        .then((res) => res.json())
+        .then((departments) => {
+            resolve(departments.map((dept) => ({
+                value: dept,
+                label: dept,
+            })))
+        })
+        .catch(reject);
+    });
+
+    useEffect(() => {
+        setMoreExists(false);
         setCourses([]);
         // @ts-ignore Urls is declared in the django template
-        setNextUrl(urlArg ? `${Urls.courseList()}?${urlArg}` : Urls.courseList());
+        setNextUrl(`${Urls.courseList()}?${urlParams.toString()}`)
         setMoreExists(true);
-        setActiveTab(tabNumber)
+    }, [urlParams]);
+
+    const handleChangeTab = (tab) => () => {
+        if (tab != activeTab) {
+            if (tab == tabs.ONLY_ENROLLED) {
+                setUrlParams((prev) => {
+                    prev.set('is_enrolled', 'true');
+                    return new URLSearchParams(prev);
+                })
+            } else {
+                setUrlParams((prev) => {
+                    prev.delete('is_enrolled');
+                    return new URLSearchParams(prev);
+                })
+            }
+            setActiveTab(tab);
+        }
+    }
+
+    const handleDepartmentFilter = (selectedDepartment) => {
+        if (selectedDepartment.length > 0) {
+            setUrlParams((prev) => {
+                prev.set('department', selectedDepartment.map(({ value }) => (value)).join(","));
+                return new URLSearchParams(prev);
+            });
+        } else {
+            setUrlParams((prev) => {
+                prev.delete('department');
+                return new URLSearchParams(prev);
+            });
+        }
     }
 
     return (
         <>
+            <AsyncSelect
+                isMulti
+                isClearable
+                isSearchable={false}
+                cacheOptions
+                defaultOptions
+                loadOptions={getDepartments}
+                onChange={handleDepartmentFilter}
+                placeholder="Filtrer par départements"
+                styles={selectStyle}
+            />
             <div className="row tab">
                 <div className="col">
-                    <button className={activeTab == 1 ? "active" : ""} onClick={handleChangeTab(1)} type="button">
+                    <button className={activeTab == tabs.ALL_COURSE ? "active" : ""} onClick={handleChangeTab(tabs.ALL_COURSE)} type="button">
                             Tous les cours
                     </button>
                 </div>
                 <div className="col">
-                    <button className={activeTab == 2 ? "active": ""} onClick={handleChangeTab(2, "is_enrolled=true")} type="button">
+                    <button className={activeTab == tabs.ONLY_ENROLLED ? "active": ""} onClick={handleChangeTab(tabs.ONLY_ENROLLED)} type="button">
                         Mes cours
                     </button>
                 </div>
