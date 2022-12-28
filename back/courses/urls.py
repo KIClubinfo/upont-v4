fdbc17555601798f944afb478e4ad2d897b81b09
Ludovic Druette <ludovicdruette@protonmail.com>
2022-12-28 17:58:41 +0100

    remove the code that refere to the courses details page

--- back/courses/urls.py
+++ back/courses/urls.py
@@ -1,10 +1,3 @@
 app_name = "course"
 
-from django.urls import path
-
-from . import views
-
-
-urlpatterns = [
-       path("course/<int:course_id>/details", views.view_course, name="course_detail"),
-]
+urlpatterns = []
