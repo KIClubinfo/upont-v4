37c2da4d2e8424ab3bece34f28cff9aaabd3ec49
Ludovic Druette <ludovicdruette@protonmail.com>
2022-12-28 17:58:03 +0100

    Correct a few mistakes

--- back/courses/models.py
+++ back/courses/models.py
@@ -1,17 +1,13 @@
-from django.core.files import File
 from django.db import models
 
 
-
 class Ressources(models.Model):
-    name=models.CharField(max_length=50, default="Ressource")
+    name = models.CharField(max_length=50, default="Ressource")
     author = models.ForeignKey(
         "social.Student", verbose_name="author", on_delete=models.SET_NULL, null=True
     )
     date = models.DateTimeField()
-    file= FileField(
-        "Fichier",upload_to='ressources', null=TRUE,blank=TRUE
-        )
+    file = models.FileField("Fichier", upload_to="ressources", null=True, blank=True)
     post = models.ForeignKey(
         "news.Post", verbose_name="post", on_delete=models.SET_NULL, null=True
     )
