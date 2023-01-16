66b930fdf87d15b44cc87005a84ebb25930f6984
Ludovic Druette <ludovicdruette@protonmail.com>
2023-01-16 12:49:46 +0100

    put the number of days to scrap as a parameter in the associated task

--- back/courses/tasks.py
+++ back/courses/tasks.py
@@ -9,16 +9,18 @@ from .scrapper import get_schedule
 
 
 @app.task
-def update_timeslots():
+def update_timeslots(days_forward):
     """
     Daily run tasks to update courses timeslots with informations from
     emploidutemps.enpc.fr
+
+    days_forward is the number of day to update, starting from today
     """
     today = timezone.now().date()
 
     Timeslot.objects.filter(start__lte=today - datetime.timedelta(60))
 
-    for i in range(200):
+    for i in range(days_forward):
         get_schedule(today + datetime.timedelta(days=i))
 
 
@@ -26,5 +28,7 @@ def update_timeslots():
 def setup_periodic_tasks(sender, **kwargs):
     # Calls update_timeslots every day at midnight
     sender.add_periodic_task(
-        crontab(minute=0, hour=0), update_timeslots.s(), name="Update courses timeslots"
+        crontab(minute=0, hour=0),
+        update_timeslots.s(200),
+        name="Update courses timeslots",
     )
