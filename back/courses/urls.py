162e9b886beb47522db9997185b12defeb79a115
audreyairaud <audrey.airaud@gmail.com>
2023-02-13 10:10:56 +0100

    add packages

--- back/courses/urls.py
+++ back/courses/urls.py
@@ -1,4 +1,19 @@
+import csv
+import io
+
+from django.contrib.auth import models as models
+from django.contrib.auth.decorators import login_required
+from django.core.exceptions import PermissionDenied
+from django.http import HttpResponse, HttpResponseForbidden, HttpResponseRedirect
+from django.shortcuts import render
+from django.urls import reverse
+from social.models import Promotion, Student
+
+from .settings import LOGIN_REDIRECT_URL, LOGIN_URL
+
+
 app_name = "course"
+
 
 urlpatterns = [
     path("add_course/", views.add, name="add_course"),
