74fcadd571bab7ee723051f558da57cec5faabc8
adcrry <ducorroyalexandre@gmail.com>
2023-07-31 17:06:53 +0200

    add active students list in club serializer

--- back/social/models.py
+++ back/social/models.py
@@ -170,6 +170,9 @@ class Club(models.Model):
 
     def balance_in_euros(self):
         return self.balance_in_cents() / 100
+    
+    def getMembers(self):
+        return self.members.filter(membership__is_old=False)
 
 
 class Membership(models.Model):
