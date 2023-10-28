2ff994bf18f66d3daf6a3c6f4d506c05c427ccc1
adcrry <ducorroyalexandre@gmail.com>
2023-10-28 22:22:05 +0200

    add regex parser

--- back/news/views.py
+++ back/news/views.py
@@ -1,4 +1,5 @@
 import json
+import re
 
 from courses.models import Course, Resource
 from django.contrib.auth.decorators import login_required
@@ -444,6 +445,48 @@ def post_edit(request, post_id, course_id=None):
     return render(request, "news/post_edit.html", context)
 
 
+def test_for_markdown_link(text):
+    for i in range(len(text)):
+        if text[i] == "[":
+            for j in range(i, len(text)):
+                if text[j] == "]":
+                    if len(text) > j + 1 and text[j + 1] == "(":
+                        for k in range(j + 1, len(text)):
+                            if text[k] == ")":
+                                return (i, k)
+    return False
+
+
+def split_then_markdownify(text):
+    if test_for_markdown_link(text):
+        i, k = test_for_markdown_link(text)
+        return (
+            split_then_markdownify(text[:i])
+            + text[i : k + 1]
+            + split_then_markdownify(text[k + 1 :])
+        )
+    else:
+        return convert_to_markdown(text)
+
+
+def convert_to_markdown(text):
+    url_pattern = r"http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\\(\\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+"
+    markdown_pattern = r"\[.*?\]\(.*?\)"
+    url_regex = re.compile(url_pattern)
+    markdown_regex = re.compile(markdown_pattern)
+
+    def replace_url(match):
+        url = match.group()
+        if not markdown_regex.match(url):
+            markdown_url = f"[{url}]({url})"
+            return markdown_url
+        else:
+            return url
+
+    text = url_regex.sub(replace_url, text)
+    return text
+
+
 @login_required
 def post_create(request, event_id=None, course_id=None):
     context = {}
@@ -454,11 +497,11 @@ def post_create(request, event_id=None, course_id=None):
                 request.POST,
                 request.FILES,
             )
-            print(request.POST)
             if form.is_valid():
                 post = form.save(commit=False)
                 post.author = Student.objects.get(user__id=request.user.id)
                 post.date = timezone.now()
+                post.content = convert_to_markdown(post.content)
                 post.save()
                 if course_id is not None:
                     course = get_object_or_404(Course, pk=course_id)
