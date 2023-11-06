db929795fc8088a154b451bdc93dc9b9e70767df
Nicolas Bessin <nicolasbessin49@gmail.com>
2023-11-06 16:12:50 +0100

    Added minimal views for baskets

--- back/epicerie/urls.py
+++ back/epicerie/urls.py
@@ -4,4 +4,6 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path("panier/", views.basket, name="panier"),
+    path("panier/<int:basket_id>/", views.basket_detail, name="panier_detail"),
 ]
--- back/epicerie/views.py
+++ back/epicerie/views.py
@@ -5,4 +5,10 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    return HttpResponse("Hello, world. You're at the epicerie index.")
+
+def basket(request):
+    return HttpResponse("Hello, world. You're at the basket index.")
+
+def basket_detail(request, basket_id):
+    return HttpResponse(f"This is basket {basket_id}")
