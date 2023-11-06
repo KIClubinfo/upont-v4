a6a599e35e26fc57565175c0901b11b256a476ef
Nicolas Bessin <nicolasbessin49@gmail.com>
2023-11-06 16:27:41 +0100

    replaced \n with ,

--- back/epicerie/views.py
+++ back/epicerie/views.py
@@ -11,7 +11,7 @@ def index(request):
 
 def basket(request):
     basket_list = Basket.objects.filter(is_active=True)
-    output = '\n '.join([str(basket) for basket in basket_list])
+    output = ', '.join([str(basket) for basket in basket_list])
     return HttpResponse(output)
 
 def basket_detail(request, basket_id):
