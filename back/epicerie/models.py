b5f02336eea7d01ac0d0721ba6e341e5c93fb5cf
Juliette-Angue <juliette.angue@eleves.enpc.fr>
2023-11-06 15:36:28 +0100

    création modèle panier

--- back/epicerie/models.py
+++ back/epicerie/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
 # Create your models here.
+
+class Basket(models.Model):
+    price = models.IntegerField(default=0)          # in cents
+    composition = models.TextField()
+    open_date = models.DateTimeField()
+    close_date = models.DateTimeField()
+    pickup_date = models.DateTimeField()
+    is_active = models.BooleanField(default=True)
+
+    def __str__(self):
+        return f"Panier à {self.price/100}€ du {self.pickup_date}" 
+
+class Basket_Order(models.Model):
+    basket = models.ForeignKey(Basket, on_delete=models.SET_NULL, null=True)
+    student = models.ForeignKey('social.Student', on_delete=models.SET_NULL, null=True)
+    quantity = models.IntegerField(default=0)
+
+    def __str__(self):
+        return str(self.basket) + " " + str(self.student)
