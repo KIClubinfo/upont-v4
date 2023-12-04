4637e62a543a4a6e0f8802ff430684b26a89516b
Juliette-Angue <juliette.angue@eleves.enpc.fr>
2023-12-04 14:14:51 +0100

    ecriture api pour le vrac dans fichier serializers.py

--- back/epicerie/serializers.py
+++ back/epicerie/serializers.py
@@ -1,8 +1,56 @@
-from rest_framework.serializers import ModelSerializer 
-from shop.models import Category
- 
-class CategorySerializer(ModelSerializer):
- 
-    class Product:
-        model = Category
-        fields = ['name', 'step', 'max', 'price']
+from rest_framework import serializers
+
+from social.serializers import UserSerializer
+
+from .models import Product, Vrac, Vrac_Order
+
+
+class ProductSerializer(serializers.ModelSerializer):
+    composition = serializers.SerializerMethodField()
+
+    def get_composition(self, obj):
+        return [str(vegetable) for vegetable in obj.composition.all()]
+    
+    class Meta:
+        model = Product
+        fields = [
+            "name",
+            "step",
+            "max",
+            "price",
+        ]
+
+class BasketSerializerLite(serializers.ModelSerializer):
+    
+    class Meta:
+        model = Basket
+        fields = [
+            "id",
+            "price",
+            "pickup_date",
+
+        ]
+       
+class BasketOrderSerializer(serializers.ModelSerializer):
+
+    student = serializers.SerializerMethodField()
+
+    def get_student(self, obj):
+        simplified_student = {}
+        simplified_student["id"] = obj.student.id
+        simplified_student["first_name"] = obj.student.user.first_name
+        simplified_student["last_name"] = obj.student.user.last_name
+        simplified_student["email"] = obj.student.user.email
+        simplified_student["phone_number"] = obj.student.phone_number
+        return simplified_student
+    
+    basket = BasketSerializerLite()
+    
+    class Meta:
+        model = Basket_Order
+        fields = [
+            "id",
+            "student",
+            "basket",
+            "quantity",
+        ]
