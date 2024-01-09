34249edec61b6685f55e2bc969cfe8868f742766
Nicolas-Bessin <nicolasbessin49@gmail.com>
2024-01-09 22:08:22 +0100

    working on the full display o ingredients in a basket

--- react/src/components/EpiceriePage/AdminBasket.tsx
+++ react/src/components/EpiceriePage/AdminBasket.tsx
@@ -21,18 +21,17 @@ const deleteVegetable = (id : number) => {
 const Composition : React.FC<BasketProp> = ( prop  : BasketProp) => {
     return (
         <div>
-            <p>Composition du panier</p>
-            <ul>
-                {prop.basket.composition.map((item) => (
-                    <li key={item.id}>
-                        {item.name} {item.quantity} g
-                        <button className="button red-button"
-                            onClick={() => deleteVegetable(item.id)}>
-                            Supprimer
-                        </button>
-                    </li>
-                ))}
-            </ul>
+                <ul className="flex-list">
+                    {prop.basket.composition.map((item) => (
+                        <li key={item.id}>
+                            {item.name} {item.quantity} g
+                            <button className="button red-button"
+                                onClick={() => deleteVegetable(item.id)}>
+                                Supprimer
+                            </button>
+                        </li>
+                    ))}
+                </ul>
         </div>
     );
 }
@@ -78,10 +77,10 @@ const AdminBasket = () => {
     }
 
     return (
-        <div className="row row-cols-2">
-            <div className="col">
-                <div className="epicerie-card">
-                    <div className="epicerie-card-title">
+        <div className="row">
+            <div className="col-8">
+                <div className="epicerie-wide-box">
+                    <div className="epicerie-card-title centered-div">
                         <span>
                             Panier à {baskets[index].price / 100}€ du {new Date(baskets[index].pickup_date).toLocaleDateString("fr-FR")}
                         </span>
@@ -90,13 +89,25 @@ const AdminBasket = () => {
                         <Composition basket={baskets[index]} />
                     </div>
 
-                    <div className="epicerie-card-button">
+                    <div className="centered-div epicerie-card-button">
                         <div>
                             <button className="button blue-button" onClick={previousBasket}> Précédent </button>
                             <button className="button blue-button" onClick={nextBasket}> Suivant </button>
                         </div>
                     </div>
-                </div>  
+                </div>
+            </div>
+
+            <div className="col-4">
+                <div className="epicerie-card">
+                    <div className="epicerie-card-title">
+                        <span>
+                            Ajouter un panier
+                        </span>
+                    </div>  
+                    <div className="epicerie-card-content">
+                    </div>
+                </div>
             </div>
                 
         </div>
